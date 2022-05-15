e0f99af7dc65e37d6b7aa7e85b701be042bba2e9
elnar <elnarasabira@gmal.com>
2022-05-15 09:05:21 -0700

    the red Audii car is ready with some basic features

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,12 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        basicFeatures car =new basicFeatures();
-
+        basicFeatures Auddi =new basicFeatures();
+        Auddi .Color="red";
+        Auddi.doors="4";
+        Auddi.engineCapac="v8";
+        Auddi .headlights="blue";
+        Auddi.regNo="tuzik";
+       
     }
 }
